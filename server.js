e7ff661eef7ba3105b400d6f63aff2c638a1a625
Ceka124124 <rowqa08@gmail.com>
2025-04-09 16:09:47 +0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Sunucu ${port} portunda çalışıyor`);
+});
 
 const app = express();
 const server = http.createServer(app);
