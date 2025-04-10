0c896ff47e8c476caf66de42ab93bf0d75f51404
Ceka124124 <rowqa08@gmail.com>
2025-04-11 01:32:48 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors'); // CORS modülünü ekle
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Statik dosyaları sunma
+// CORS ayarlarını yap
+app.use(cors());
+
+// Statik dosyaların sunulması (HTML, CSS, JS)
 app.use(express.static('public'));
+
+// Anasayfaya gelen istekler için index.html dosyasını sunma
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 // Socket.io bağlantısı
 io.on('connection', (socket) => {
